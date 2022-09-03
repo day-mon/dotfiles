c10ca87c9d408c1bc6a6edf4cdef3c3ab3044a9a
Damon Montague <damon@montague.im>
2022-09-02 22:30:03 -0400

    added new lua based nvim config with cool new plugins

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+-- lua at the beginning and end is implied
+-- if there is a folder it will look for init.lua
+require "damon.options"
+require "damon.keymaps"
+require "damon.plugins"
+require "damon.colorscheme"
+require "damon.cmp"
+require "damon.lsp"
+require "damon.telescope"
+require "damon.treesitter"
+require "damon.gitsigns"
+require "damon.nvim-tree"
