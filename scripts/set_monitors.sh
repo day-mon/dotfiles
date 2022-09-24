7322fb2de839562eda8b9ef2a4252822f02fbb85
damon <damon@montague.im>
2022-09-24 15:11:47 -0400

    pushing scripts

--- scripts/set_monitors.sh
+++ scripts/set_monitors.sh
@@ -1,4 +1,18 @@
+#!/bin/sh
+REFRESH_RATE="$1"
+if [[ -z "$REFRESH_RATE" ]]; 
+then
+	REFRESH_RATE="164.83"
+	echo "Input was blank setting refresh rate to $REFRESH_RATE by default"
+fi
+
+if ! [[ "$REFRESH_RATE" =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]; 
+then 
+    echo "Inputs must be a numbers | Input $REFRESH_RATE" 
+    exit 0 
+fi
+
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    xrandr --output "$m" --rate 164.83 --mode 2560x1440     
-    echo "Monitor $m set to 164.83 hz"
+    xrandr --output "$m" --rate "$REFRESH_RATE" --mode 2560x1440     
+    echo "Monitor $m set to $REFRESH_RATE hz"
 done
