146017c4c6942c411fbc4fa7e274b2d8e959f6f7
damon <damon@montague.im>
2022-10-01 16:01:28 -0400

    added new stuff

--- scripts/set_monitors.sh
+++ scripts/set_monitors.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
-REFRESH_RATE="$1"
-if [[ -z "$REFRESH_RATE" ]]; 
-then
-	REFRESH_RATE="164.83"
-	echo "Input was blank setting refresh rate to $REFRESH_RATE by default"
-fi
+MONITOR_SIZE=""
+RATE=""
+OUTPUT=""
+I=1
+for m in $(xrandr --query | grep -A1 " connected" | awk '{print $1; print $4}' | sed "s/*//g;" | sed "s/(normal//g;" | sed "s/--//g" | sed '/^[[:space:]]*$/d'); do
 
-if ! [[ "$REFRESH_RATE" =~ ^[+-]?[0-9]+\.?[0-9]*$ ]]; 
-then 
-    echo "Inputs must be a numbers | Input $REFRESH_RATE" 
-    exit 0 
-fi
+    if [ $I -eq  1 ]; then
+    	OUTPUT=$m
+    fi
 
-for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    xrandr --output "$m" --rate "$REFRESH_RATE" --mode 2560x1440     
-    echo "Monitor $m set to $REFRESH_RATE hz"
+    if [ $I -eq 2 ]; then
+    	MONITOR_SIZE=$m
+    fi
+
+    if [ $I -eq 3 ]; then
+	    RATE=$m
+	    xrandr --output "$OUTPUT" --mode "$MONITOR_SIZE" --rate "$RATE"
+	    echo "Setting $OUTPUT to $MONITOR_SIZE at $RATE"
+	    I=0
+	    MONITOR_SIZE=""
+	    RATE=""
+	    OUTPUT=""
+    fi
+
+   I=$((I=I+1))
 done
