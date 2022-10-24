cd5c69425d2729d489e3a4ff1091c0b49d5f962f
Damon L Montague Jr <damon@montague.im>
2022-10-24 19:44:33 -0400

    dots update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,23 +1,81 @@
 #!/usr/bin/env bash
 
-# Add this script to your wm startup file.
+dir="$HOME/.config/polybar"
+themes=(`ls --hide="launch.sh" $dir`)
 
-DIR="$HOME/.config/polybar/shades"
+launch_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
 
-# Terminate already running bar instances
-killall -q polybar
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+	# Launch the bar
+	if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
+		polybar -q top -c "$dir/$style/config.ini" &
+		polybar -q bottom -c "$dir/$style/config.ini" &
+	elif [[ "$style" == "pwidgets" ]]; then
+		bash "$dir"/pwidgets/launch.sh --main
+	else
+		polybar -q main -c "$dir/$style/config.ini" &	
+	fi
+}
 
-PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
-OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+if [[ "$1" == "--material" ]]; then
+	style="material"
+	launch_bar
 
-# Launch on primary monitor
-MONITOR=$PRIMARY polybar --reload -c $DIR/config.ini &
-sleep 1
+elif [[ "$1" == "--shades" ]]; then
+	style="shades"
+	launch_bar
 
-# Launch on all other monitors
-for m in $OTHERS; do
- MONITOR=$m polybar --reload -c $DIR/config.ini &
-done
+elif [[ "$1" == "--hack" ]]; then
+	style="hack"
+	launch_bar
+
+elif [[ "$1" == "--docky" ]]; then
+	style="docky"
+	launch_bar
+
+elif [[ "$1" == "--cuts" ]]; then
+	style="cuts"
+	launch_bar
+
+elif [[ "$1" == "--shapes" ]]; then
+	style="shapes"
+	launch_bar
+
+elif [[ "$1" == "--grayblocks" ]]; then
+	style="grayblocks"
+	launch_bar
+
+elif [[ "$1" == "--blocks" ]]; then
+	style="blocks"
+	launch_bar
+
+elif [[ "$1" == "--colorblocks" ]]; then
+	style="colorblocks"
+	launch_bar
+
+elif [[ "$1" == "--forest" ]]; then
+	style="forest"
+	launch_bar
+
+elif [[ "$1" == "--pwidgets" ]]; then
+	style="pwidgets"
+	launch_bar
+
+elif [[ "$1" == "--panels" ]]; then
+	style="panels"
+	launch_bar
+
+else
+	cat <<- EOF
+	Usage : launch.sh --theme
+		
+	Available Themes :
+	--blocks    --colorblocks    --cuts      --docky
+	--forest    --grayblocks     --hack      --material
+	--panels    --pwidgets       --shades    --shapes
+	EOF
+fi
