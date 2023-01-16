90053201542345b788f6d16322c7a236ee29a6d5
Damon L Montague Jr <damon@montague.im>
2023-01-16 02:48:23 -0500

    New changes - Realiased rm because I deleted a whole project - Updated kitty conf - Other random stuff

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,11 +1,11 @@
-local status_okay, bultin = pcall(require, 'telescope.builtin')
+local status_okay, builtin = pcall(require, 'telescope.builtin')
 if not status_okay then
 	vim.notify('Oopsies telescope isnt working')
-	vim.notify(bultin)
+	vim.notify(builtin)
 	return
 end
 
-vim.keymap.set('n', '<leader>pf', bultin.find_files, {})
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function() 
 	builtin.grep_string({ search = vim.fn.input(" Grep > ") });
 end)
