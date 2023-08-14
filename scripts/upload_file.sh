cf3435ece4230cc9890b577f10b3777e11154adb
Damon L. Montague Jr <damon@montague.im>
2023-08-13 22:58:19 -0400

    new stuff

--- scripts/upload_file.sh
+++ scripts/upload_file.sh
@@ -4,14 +4,14 @@ URL="https://upload.montague.im"
 
 . "$HOME"/.config/zsh/secrets
 
-if [ -z $SCREENSHOT_UPLOAD_AUTH ]; then
-    notify-send "❌ Please set the SCREENSHOT_UPLOAD_AUTH variable in $HOME/.config/zsh/secrets"
+if [ -z UPLOAD_TOKEN ]; then
+    notify-send "❌ Please set the UPLOAD_TOKEN variable in $HOME/.config/zsh/secrets"
     exit
 fi
 
 for f in $ALL_FILES
 do
-  UPLOAD=$(curl -H "Content-Type: multipart/form-data" -H "authorization: ""${SCREENSHOT_UPLOAD_AUTH}""" -F file="$f" ${URL}/api/upload)
+  UPLOAD=$(curl -H "Content-Type: multipart/form-data" -H "authorization: ""${UPLOAD_TOKEN}""" -F file="$f" ${URL}/api/upload)
   if [ $? -eq 0  ]; then
     UPLOAD=$(echo $UPLOAD | jq '.files[0]' | sed "s/\"//g")
     notify-send "✅ Screenshot successfully uploaded to ${UPLOAD}"
